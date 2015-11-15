08efd11a87adbad0e53d970ce73dd1e78d582edf
Baruch Sterin <baruchs@gmail.com>
2015-11-14 23:02:42 -0800

    Add Sanitizer configuration for clang'address sanitizer

--- pyabc.cmake
+++ pyabc.cmake
@@ -15,3 +15,8 @@ set(CPACK_GENERATOR "TGZ")
 include(CPack)
 
 configure_file(${CMAKE_CURRENT_LIST_DIR}/cpack_project_file.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/cpack_project_file.cmake @ONLY)
+
+set(CMAKE_CXX_FLAGS_SANITIZER "-g -fsanitize=address -fno-omit-frame-pointer")
+set(CMAKE_C_FLAGS_SANITIZER "-g -fsanitize=address -fno-omit-frame-pointer")
+set(CMAKE_EXE_LINKER_FLAGS_SANITIZER -fsanitize=address)
+set(CMAKE_SHARED_LINKER_FLAGS_SANITIZER -fsanitize=address)
