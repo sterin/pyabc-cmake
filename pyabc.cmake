aade774a59d1f2edb559a390015d872004b0ceb6
Baruch Sterin <baruchs@gmail.com>
2015-11-17 10:35:47 -0800

    make it easy to disable readline, even if present on the system

--- pyabc.cmake
+++ pyabc.cmake
@@ -1,6 +1,11 @@
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 
-find_package(Readline)
+if(NOT PYABC_USE_NO_READLINE)
+    find_package(Readline)
+else()
+    set(READLINE_FOUND FALSE)
+endif()
+
 find_package(Hg REQUIRED)
 
 include(install_rules)
